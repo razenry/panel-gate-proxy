fa66999c6a24b87857949d3b519a31cd142a439f
razenry <razenry.code@gmail.com>
2026-04-21 10:06:44 +0700

    Fix Mixed Content Error over HTTPS in Dokploy by trusting all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('subscription:check')->everyFiveMinutes();
     })
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'admin' => AdminMiddleware::class,
             '2fa.enforce' => EnforceTwoFactor::class,
